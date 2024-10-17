eeaf0ddc226c5155fb90b075ced195549b47fdf6
Konrad Malarczyk <konrad.malarczyk1@gmail.com>
2024-10-17 17:25:33 +0200

    Aded logic for Login function in main

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,19 @@
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        // to see how IntelliJ IDEA suggests fixing it.i
+        boolean isLooginIn = false;
+
+
+
         Register.register();
-        Login.login();
+
+        if(isLooginIn){
+            Login.login();
+            //Logingin function
+        }
+
+
 
 
 
